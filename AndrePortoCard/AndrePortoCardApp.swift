1bcd77f13b9973f3b605b4d78ea773a38287ae2f
André Porto <andreporto@me.com>
2023-06-20 15:31:59 -0300

    Initial Commit

---
+++ AndrePortoCard/AndrePortoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndrePortoCardApp.swift
+//  AndrePortoCard
+//
+//  Created by André Porto on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AndrePortoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
